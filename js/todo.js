86a8b49472ced82d31ef6d7009330bd897dda94d
신희성 <vvwv3092@gmail.com>
2023-05-28 16:54:11 +0900

    #7.8

--- js/todo.js
+++ js/todo.js
@@ -4,7 +4,7 @@ const toDoList = document.querySelector("ul#todo-list");
 
 const TODOS_KEY = "todos";
 
-const toDos = [];
+let toDos = [];
 
 function saveToDos() {
   localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
@@ -13,8 +13,13 @@ function saveToDos() {
 function deleteToDo(e) {
   // 버튼의 부모에 접근(li태크)
   const li = e.target.parentElement;
-  // 삭제.
+  // 삭제
+  // filter로 로컬스토리지에서 가져온 toDos의 id와 누른 li의 id 같은 것을 제외하고
+  // 다시 toDos 배열에 담아 로컬스토리지에 저장한다.
+  toDos = toDos.filter((toDos) => toDos.id != Number(li.id));
   li.remove();
+
+  saveToDos();
 }
 
 function paintToDo(newToDo) {
@@ -23,13 +28,15 @@ function paintToDo(newToDo) {
   button.addEventListener("click", deleteToDo);
   // li 태그를 만듬
   const toDoLi = document.createElement("li");
+  // list의 아이디를 줌
+  toDoLi.id = newToDo.id;
   // span 태그를 만듬
   const toDoText = document.createElement("span");
   // li 태그 안에 span 태그를 넣음
   toDoLi.append(toDoText);
   toDoLi.append(button);
   // li 태그 안의 span 태그의 택스트에 newToDo 를 넣음
-  toDoText.innerText = newToDo;
+  toDoText.innerText = newToDo.text;
   // li 태그를 ul 태그 안에 넣음
   toDoList.append(toDoLi);
 }
@@ -38,8 +45,12 @@ function handleToDoSubmit(e) {
   e.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newToDo);
-  paintToDo(newToDo);
+  const newTodoObj = {
+    text: newToDo,
+    id: Date.now(),
+  };
+  toDos.push(newTodoObj);
+  paintToDo(newTodoObj);
   saveToDos();
 }
 
@@ -49,4 +60,10 @@ const savedToDos = localStorage.getItem(TODOS_KEY);
 
 if (savedToDos) {
   const parsedToDos = JSON.parse(savedToDos);
+
+  toDos = parsedToDos;
+
+  // forEach 배열의 요소의 수 만큼 반복한다.
+  // 파라미터로 item을 준다.
+  parsedToDos.forEach(paintToDo);
 }
